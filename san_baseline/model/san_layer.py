2bccf7dbf00d687d375936d2c19512094273180e
justin@pc-justin-13900KS <justin.manson@epfl.ch>
2024-05-15 06:11:16 +0200

    add working san baseline

---
+++ san_baseline/model/san_layer.py
@@ -0,0 +1,325 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_scatter import scatter
+from torch_geometric.utils import remove_self_loops
+from torch_geometric.nn import global_mean_pool
+from torch_geometric.graphgym.register import register_head
+
+
+def negate_edge_index(edge_index, batch=None):
+    """Negate batched sparse adjacency matrices given by edge indices.
+
+    Returns batched sparse adjacency matrices with exactly those edges that
+    are not in the input `edge_index` while ignoring self-loops.
+
+    Implementation inspired by `torch_geometric.utils.to_dense_adj`
+
+    Args:
+        edge_index: The edge indices.
+        batch: Batch vector, which assigns each node to a specific example.
+
+    Returns:
+        Complementary edge index.
+    """
+
+    if batch is None:
+        batch = edge_index.new_zeros(edge_index.max().item() + 1)
+
+    batch_size = batch.max().item() + 1
+    one = batch.new_ones(batch.size(0))
+    num_nodes = scatter(one, batch, dim=0, dim_size=batch_size, reduce='add')
+    cum_nodes = torch.cat([batch.new_zeros(1), num_nodes.cumsum(dim=0)])
+
+    idx0 = batch[edge_index[0]]
+    idx1 = edge_index[0] - cum_nodes[batch][edge_index[0]]
+    idx2 = edge_index[1] - cum_nodes[batch][edge_index[1]]
+
+    negative_index_list = []
+    for i in range(batch_size):
+        n = num_nodes[i].item()
+        size = [n, n]
+
+        # THIS SECTION (scatter() OPERATION) LED TO SEGMENTATION FAULT
+
+        # adj = torch.ones(size, dtype=torch.short, device=edge_index.device)
+
+        # Remove existing edges from the full N x N adjacency matrix
+        # flattened_size = n * n
+        # adj = adj.view([flattened_size])
+        # _idx1 = idx1[idx0 == i]
+        # _idx2 = idx2[idx0 == i]
+        # idx = _idx1 * n + _idx2
+        # zero = torch.zeros(_idx1.numel(), dtype=torch.short,
+        #                    device=edge_index.device)
+        # scatter(zero, idx, dim=0, out=adj, reduce='mul')
+
+        # adj = adj.view(size)
+
+        # ALTERNATIVE IMPLEMENTATION USING BINARY MASK
+
+        # Create a mask for existing edges
+        adj = torch.ones(size, dtype=torch.bool, device=edge_index.device)
+        _idx1 = idx1[idx0 == i]
+        _idx2 = idx2[idx0 == i]
+
+        # Remove existing edges
+        adj[_idx1, _idx2] = False
+
+
+        # Convert to edge index format
+        _edge_index = adj.nonzero(as_tuple=False).t().contiguous()
+        _edge_index, _ = remove_self_loops(_edge_index)
+        negative_index_list.append(_edge_index + cum_nodes[i])
+
+    edge_index_negative = torch.cat(negative_index_list, dim=1).contiguous()
+    return edge_index_negative
+
+
+class MultiHeadAttentionLayer(nn.Module):
+    """Multi-Head Graph Attention Layer.
+
+    Ported to PyG from original repo:
+    https://github.com/DevinKreuzer/SAN/blob/main/layers/graph_transformer_layer.py
+    """
+
+    def __init__(self, gamma, in_dim, out_dim, num_heads, full_graph,
+                 fake_edge_emb, use_bias):
+        super().__init__()
+
+        self.out_dim = out_dim
+        self.num_heads = num_heads
+        self.gamma = gamma
+        self.full_graph = full_graph
+
+        self.Q = nn.Linear(in_dim, out_dim * num_heads, bias=use_bias)
+        self.K = nn.Linear(in_dim, out_dim * num_heads, bias=use_bias)
+        self.E = nn.Linear(in_dim, out_dim * num_heads, bias=use_bias)
+
+        if self.full_graph:
+            self.Q_2 = nn.Linear(in_dim, out_dim * num_heads, bias=use_bias)
+            self.K_2 = nn.Linear(in_dim, out_dim * num_heads, bias=use_bias)
+            self.E_2 = nn.Linear(in_dim, out_dim * num_heads, bias=use_bias)
+            self.fake_edge_emb = fake_edge_emb
+
+        self.V = nn.Linear(in_dim, out_dim * num_heads, bias=use_bias)
+
+    def propagate_attention(self, batch):
+        src = batch.K_h[batch.edge_index[0]]  # (num real edges) x num_heads x out_dim
+        dest = batch.Q_h[batch.edge_index[1]]  # (num real edges) x num_heads x out_dim
+        score = torch.mul(src, dest)  # element-wise multiplication
+
+        # Scale scores by sqrt(d)
+        score = score / np.sqrt(self.out_dim)
+
+        if self.full_graph:
+            fake_edge_index = negate_edge_index(batch.edge_index, batch.batch)
+            src_2 = batch.K_2h[fake_edge_index[0]]  # (num fake edges) x num_heads x out_dim
+            dest_2 = batch.Q_2h[fake_edge_index[1]]  # (num fake edges) x num_heads x out_dim
+            score_2 = torch.mul(src_2, dest_2)
+
+            # Scale scores by sqrt(d)
+            score_2 = score_2 / np.sqrt(self.out_dim)
+
+        # Use available edge features to modify the scores for edges
+        score = torch.mul(score, batch.E)  # (num real edges) x num_heads x out_dim
+
+        if self.full_graph:
+            # E_2 is 1 x num_heads x out_dim and will be broadcast over dim=0
+            score_2 = torch.mul(score_2, batch.E_2)
+
+        if self.full_graph:
+            # softmax and scaling by gamma
+            score = torch.exp(score.sum(-1, keepdim=True).clamp(-5, 5))  # (num real edges) x num_heads x 1
+            score_2 = torch.exp(score_2.sum(-1, keepdim=True).clamp(-5, 5))  # (num fake edges) x num_heads x 1
+            score = score / (self.gamma + 1)
+            score_2 = self.gamma * score_2 / (self.gamma + 1)
+        else:
+            score = torch.exp(score.sum(-1, keepdim=True).clamp(-5, 5))  # (num real edges) x num_heads x 1
+
+        # Apply attention score to each source node to create edge messages
+        msg = batch.V_h[batch.edge_index[0]] * score  # (num real edges) x num_heads x out_dim
+        # Add-up real msgs in destination nodes as given by batch.edge_index[1]
+        batch.wV = torch.zeros_like(batch.V_h)  # (num nodes in batch) x num_heads x out_dim
+        scatter(msg, batch.edge_index[1], dim=0, out=batch.wV, reduce='add')
+
+        if self.full_graph:
+            # Attention via fictional edges
+            msg_2 = batch.V_h[fake_edge_index[0]] * score_2
+            # Add messages along fake edges to destination nodes
+            scatter(msg_2, fake_edge_index[1], dim=0, out=batch.wV, reduce='add')
+
+        # Compute attention normalization coefficient
+        batch.Z = score.new_zeros(batch.size(0), self.num_heads, 1)  # (num nodes in batch) x num_heads x 1
+        scatter(score, batch.edge_index[1], dim=0, out=batch.Z, reduce='add')
+        if self.full_graph:
+            scatter(score_2, fake_edge_index[1], dim=0, out=batch.Z, reduce='add')
+
+
+    def forward(self, batch):
+        Q_h = self.Q(batch.x)
+        K_h = self.K(batch.x)
+        E = self.E(batch.edge_attr)
+
+        if self.full_graph:
+            Q_2h = self.Q_2(batch.x)
+            K_2h = self.K_2(batch.x)
+            # One embedding used for all fake edges; shape: 1 x emb_dim
+            dummy_edge = self.fake_edge_emb(batch.edge_index.new_zeros(1))
+            E_2 = self.E_2(dummy_edge)
+
+        V_h = self.V(batch.x)
+
+        # Reshaping into [num_nodes, num_heads, feat_dim] to
+        # get projections for multi-head attention
+        batch.Q_h = Q_h.view(-1, self.num_heads, self.out_dim)
+        batch.K_h = K_h.view(-1, self.num_heads, self.out_dim)
+        batch.E = E.view(-1, self.num_heads, self.out_dim)
+
+        if self.full_graph:
+            batch.Q_2h = Q_2h.view(-1, self.num_heads, self.out_dim)
+            batch.K_2h = K_2h.view(-1, self.num_heads, self.out_dim)
+            batch.E_2 = E_2.view(-1, self.num_heads, self.out_dim)
+
+        batch.V_h = V_h.view(-1, self.num_heads, self.out_dim)
+
+        self.propagate_attention(batch)
+
+        h_out = batch.wV / (batch.Z + 1e-6)
+
+        return h_out
+
+
+class SANLayer(nn.Module):
+    """GraphTransformerLayer from SAN.
+
+    Ported to PyG from original repo:
+    https://github.com/DevinKreuzer/SAN/blob/main/layers/graph_transformer_layer.py
+    """
+
+    def __init__(self, gamma, in_dim, out_dim, num_heads, full_graph,
+                 fake_edge_emb, dropout=0.0,
+                 layer_norm=False, batch_norm=True,
+                 residual=True, use_bias=False):
+        super().__init__()
+
+        self.in_channels = in_dim
+        self.out_channels = out_dim
+        self.num_heads = num_heads
+        self.dropout = dropout
+        self.residual = residual
+        self.layer_norm = layer_norm
+        self.batch_norm = batch_norm
+        self.attention = MultiHeadAttentionLayer(gamma=gamma,
+                                                 in_dim=in_dim,
+                                                 out_dim=out_dim // num_heads,
+                                                 num_heads=num_heads,
+                                                 full_graph=full_graph,
+                                                 fake_edge_emb=fake_edge_emb,
+                                                 use_bias=use_bias)
+
+        self.O_h = nn.Linear(out_dim, out_dim)
+
+        if self.layer_norm:
+            self.layer_norm1_h = nn.LayerNorm(out_dim)
+
+        if self.batch_norm:
+            self.batch_norm1_h = nn.BatchNorm1d(out_dim)
+
+        # FFN for h
+        self.FFN_h_layer1 = nn.Linear(out_dim, out_dim * 2)
+        self.FFN_h_layer2 = nn.Linear(out_dim * 2, out_dim)
+
+        if self.layer_norm:
+            self.layer_norm2_h = nn.LayerNorm(out_dim)
+
+        if self.batch_norm:
+            self.batch_norm2_h = nn.BatchNorm1d(out_dim)
+
+    def forward(self, batch):
+        h = batch.x
+        h_in1 = h  # for first residual connection
+
+        # multi-head attention out
+        h_attn_out = self.attention(batch)
+
+        # Concat multi-head outputs
+        h = h_attn_out.view(-1, self.out_channels)
+
+        h = F.dropout(h, self.dropout, training=self.training)
+
+        h = self.O_h(h)
+
+        if self.residual:
+            h = h_in1 + h  # residual connection
+
+        if self.layer_norm:
+            h = self.layer_norm1_h(h)
+
+        if self.batch_norm:
+            h = self.batch_norm1_h(h)
+
+        h_in2 = h  # for second residual connection
+
+        # FFN for h
+        h = self.FFN_h_layer1(h)
+        h = F.relu(h)
+        h = F.dropout(h, self.dropout, training=self.training)
+        h = self.FFN_h_layer2(h)
+
+        if self.residual:
+            h = h_in2 + h  # residual connection
+
+        if self.layer_norm:
+            h = self.layer_norm2_h(h)
+
+        if self.batch_norm:
+            h = self.batch_norm2_h(h)
+
+        batch.x = h
+        return batch
+
+    def __repr__(self):
+        return '{}(in_channels={}, out_channels={}, heads={}, residual={})'.format(
+            self.__class__.__name__,
+            self.in_channels,
+            self.out_channels, self.num_heads, self.residual)
+
+
+class SANGraphHead(nn.Module):
+    """
+    SAN prediction head for graph prediction tasks.
+
+    Args:
+        dim_in (int): Input dimension.
+        dim_out (int): Output dimension. For binary prediction, dim_out=1.
+        L (int): Number of hidden layers.
+    """
+
+    def __init__(self, dim_in, dim_out, L=2):
+        super().__init__()
+        self.pooling_fun = global_mean_pool  # Peptides-func uses "mean" graph_pooling
+        list_FC_layers = [
+            nn.Linear(dim_in // 2 ** l, dim_in // 2 ** (l + 1), bias=True)
+            for l in range(L)]
+        list_FC_layers.append(
+            nn.Linear(dim_in // 2 ** L, dim_out, bias=True))
+        self.FC_layers = nn.ModuleList(list_FC_layers)
+        self.L = L
+
+    def _apply_index(self, batch):
+        return batch.graph_feature, batch.y
+
+    def forward(self, batch):
+        graph_emb = self.pooling_fun(batch.x, batch.batch)
+        for l in range(self.L):
+            graph_emb = self.FC_layers[l](graph_emb)
+            graph_emb = F.relu(graph_emb)
+        graph_emb = self.FC_layers[self.L](graph_emb)
+        batch.graph_feature = graph_emb
+        pred, label = self._apply_index(batch)
+        return pred, label
+    
+
+register_head('san_graph', SANGraphHead)
---
+++ san_baseline/san_baseline.py
@@ -0,0 +1,177 @@
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import dgl
+from torch_scatter import scatter
+from torch_geometric.utils import remove_self_loops
+import torch_geometric.graphgym.register as register
+from torch_geometric.nn import GCNConv, global_mean_pool
+from torch_geometric.data import Data
+from torch_geometric.datasets import LRGBDataset
+from torch_geometric.loader import DataLoader
+from torch.optim import AdamW, Optimizer
+from torch_geometric.data import Dataset
+from tqdm import tqdm
+from sklearn.metrics import average_precision_score
+import numpy as np
+from torch_geometric.graphgym.config import (cfg, dump_cfg,
+                                             set_cfg, load_cfg,
+                                             makedirs_rm_exist) 
+from torch_geometric.graphgym.model_builder import create_model
+from torch_geometric.utils import to_scipy_sparse_matrix, get_laplacian
+from functools import partial
+from transform.posenc_stats import get_lap_decomp_stats
+
+# import custom configs
+from config import *
+from model import *
+from encoder import *
+from transform import *
+    
+def train(model: torch.nn.Module, loader: DataLoader, optimizer: Optimizer, device: torch.device) -> float:
+    model.train()
+    total_loss = 0.0
+    loss_function = torch.nn.BCEWithLogitsLoss()
+    for data in tqdm(loader):
+        data = data.to(device)
+        optimizer.zero_grad()
+        out, _ = model(data)  # the SANGraphHead module returns a pred, label tuple
+        loss = loss_function(out, data.y.float())
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+    return total_loss / len(loader)
+
+def test(model: torch.nn.Module, loader: DataLoader, device: torch.device) -> float:
+    model.eval()
+    all_preds = []
+    all_labels = []
+    for data in tqdm(loader):
+        data = data.to(device)
+        out, _ = model(data)  # the SANGraphHead module returns a pred, label tuple
+        preds = torch.sigmoid(out).detach().cpu().numpy()
+        labels = data.y.detach().cpu().numpy()
+        all_preds.append(preds)
+        all_labels.append(labels)
+    
+    all_preds = np.vstack(all_preds)
+    all_labels = np.vstack(all_labels)
+    # Calculate mean Average Precision
+    ap_scores = [average_precision_score(all_labels[:, i], all_preds[:, i]) for i in range(all_labels.shape[1])]
+    mean_ap = np.mean(ap_scores)
+    return mean_ap
+
+
+class Args:
+    def __init__(self, cfg_file):
+        self.cfg_file = cfg_file
+        self.opts = []  # You can add any additional options here if needed
+
+
+def compute_laplacian_eigen(data, normalization='sym', max_freqs=10, eigvec_norm=None):
+    """Compute eigenvalues and eigenvectors of the graph Laplacian."""
+    N = data.num_nodes if hasattr(data, 'num_nodes') else data.x.shape[0]
+    L = to_scipy_sparse_matrix(
+        *get_laplacian(data.edge_index, normalization=normalization, num_nodes=N)
+    )
+    # Below differs from original, need to make match ...
+
+    evals, evects = np.linalg.eigh(L.toarray())
+    
+    max_freqs=cfg.posenc_LapPE.eigen.max_freqs
+    eigvec_norm=cfg.posenc_LapPE.eigen.eigvec_norm
+
+    data.EigVals, data.EigVecs = get_lap_decomp_stats(
+        evals=evals, evects=evects,
+        max_freqs=max_freqs,
+        eigvec_norm=eigvec_norm
+    )
+
+    return data
+
+
+def main():
+    # For full path
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    config_file = os.path.join(script_dir, 'peptides-func-SAN.yaml')
+
+    # Hardcode the configuration file
+    # config_file = 'peptides-func-SAN.yaml'
+    args = Args(config_file)  # format to make load_cfg() work
+    
+    # Set and load config
+    set_cfg(cfg)
+    load_cfg(cfg, args)
+
+    cfg.accelerator = 'cuda' if torch.cuda.is_available() else 'cpu'  # add to config - needed for create_model()
+    device = torch.device(cfg.accelerator)  # my prefered notation
+    print(f'Using device: {device}')
+
+    pre_transform_func = partial(
+        compute_laplacian_eigen, 
+        normalization=cfg.posenc_LapPE.eigen.laplacian_norm, 
+        max_freqs=cfg.posenc_LapPE.eigen.max_freqs, 
+        eigvec_norm=cfg.posenc_LapPE.eigen.eigvec_norm
+    )
+
+    # Load training and testing sets
+    train_dataset = LRGBDataset(root="data", name="peptides-func", split='train', pre_transform=pre_transform_func)
+    test_dataset = LRGBDataset(root="data", name="peptides-func", split='test', pre_transform=pre_transform_func)
+    train_loader = DataLoader(train_dataset, batch_size=cfg.train.batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=cfg.train.batch_size, shuffle=False)
+
+    # Initialize SAN model
+    model = create_model(dim_out=10)
+
+    # Optimizer (asserts since I overwrode with my own)
+    assert cfg.optim.optimizer == "adamW", "We implement 'adamW' but cfg specifies other option"
+    assert cfg.optim.scheduler == "reduce_on_plateau", "We implement 'reduce_on_plateau' but cfg specifies other option"
+    optimizer = AdamW(model.parameters(), lr=cfg.optim.base_lr, weight_decay=0.0)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer, 
+        'min', 
+        factor=cfg.optim.reduce_factor, 
+        patience=cfg.optim.schedule_patience, 
+        min_lr=cfg.optim.min_lr
+    )
+
+    # Best model tracking
+    best_accuracy = 0
+    best_epoch = 0
+
+    # Directory for saving model checkpoints
+    model_save_dir = 'model_weights'
+    os.makedirs(model_save_dir, exist_ok=True)
+
+    early_stopping_patience = 50  # Number of epochs to wait for improvement before stopping
+    no_improve_epochs = 0
+    best_accuracy = 0
+    best_epoch = 0
+
+    for epoch in range(1, cfg.optim.max_epoch + 1):
+        loss = train(model, train_loader, optimizer, device)
+        test_acc = test(model, test_loader, device)
+        scheduler.step(test_acc)
+
+        # Early Stopping and Checkpoint Logic
+        if test_acc > best_accuracy:
+            best_accuracy = test_acc
+            best_epoch = epoch
+            no_improve_epochs = 0
+            torch.save(model.state_dict(), os.path.join(model_save_dir, f'san_weights.pt'))
+            print(f'New best model saved at epoch {epoch} with accuracy {test_acc:.4f}')
+        else:
+            no_improve_epochs += 1
+
+        if no_improve_epochs >= early_stopping_patience:
+            print(f'Early stopping triggered after {early_stopping_patience} epochs without improvement.')
+            break
+
+        print(f'Epoch: {epoch:03d}, Loss: {loss:.4f}, Accuracy: {test_acc:.4f}')
+
+    print(f'Best SAN model: Epoch {best_epoch} with Accuracy {best_accuracy:.4f}')
+
+
+if __name__ == "__main__":
+    main()
